8df0feace25b3307d71ad5490553f9bfc33f9e92
luke2327 <qwepoi8526@gmail.com>
2018-04-10 17:57:37 +0900

    FUG Table fix and added.

--- FUG/admin.py
+++ FUG/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
-from .models import FUG
+from .models import Character
 
 # Register your models here.
+class FUG_Admin(admin.ModelAdmin):
+    fieldsets = (
+        ('Infomation', {
+            'fields': ('title', 'text', 'author',)
+        }),
+        ('Advanced options', {
+            'fields': ('id', 'created_date', 'published_date', 'generation', 'post_position'),
+        })
+    )
+    list_display = (
+        'title', 'id', 'text', 'post_position', 'author', 'created_date'
+    )
+    list_filter = ('author',)
+    search_fields = ('title',)
+    ordering = ('created_date',)
 
-admin.site.register(FUG)
+    list_per_page = 20
+    
+admin.site.register(Character, FUG_Admin)
--- FUG/models.py
+++ FUG/models.py
@@ -2,17 +2,19 @@ from django.db import models
 from django.utils import timezone
 # Create your models here.
 
-class FUG(models.Model):
+class Character(models.Model):
+    # obj = Character.objects.get(pk=this_object_id)
+    id = models.AutoField(primary_key=True)
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
-    text = models.TextField()
+    text = models.TextField(default='?')
     created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
-    generation = models.IntegerField()
+    published_date = models.DateTimeField(default=timezone.now)
+    generation = models.IntegerField(default=1, blank=True)
+    post_position = models.CharField(max_length=30, null=True, default='?')
 
     def publish(self):
         self.published_date = timezone.now()
         self.save()
-
     def __str__(self):
         return self.title
